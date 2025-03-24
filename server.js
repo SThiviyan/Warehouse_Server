704ab8cdc56e2769c787ba2d129c09296618c0fa
Thiviyan <thivi30082006@outlook.com>
2025-03-24 23:28:30 +0000

    few notes

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const Fastify = require('fastify');
 const fastify = Fastify()
 
+//GET Request for retrieving record
+//POST request creates a new record
+//PUT requests updates a record
+//DELETE request deletes one
+
+
+/*
+
+REST API for the Warehouse App
+-> with JWS authentication
+-> returns JSON Format
+-> connected to a PostqreSQL Database
+
+*/
+
+
 fastify.get('/', function handler(request, reply){
     reply.send({hello: 'world'})
-})
+});
 
+
+fastify.get('/api/user', async(request, reply) => 
+{
+    return {}
+});
+
+fastify.get('/users/:id', async(request, reply)=>{
+    const newUser = {}
+});
 
 
 fastify.listen({port: 3000}, (err) => {
@@ -13,4 +38,4 @@ fastify.listen({port: 3000}, (err) => {
         fastify.log.error(err)
         process.exit(1)
     }
-})
+});
